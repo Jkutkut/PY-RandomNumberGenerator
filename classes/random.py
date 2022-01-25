c64f75ce4cd18960672734608ed158f148b6db1e
Jkutkut <jorgeregonzalez@gmail.com>
2022-01-25 16:36:46 +0100

    export

--- classes/random.py
+++ classes/random.py
@@ -1,10 +1,22 @@
 class RandomNumber:
 
-	def __init__(self, even: bool, mult: int, mini: int, maxi: int):
-		self.even = even
+	def __init__(self, mult: list, mini: int, maxi: int):
 		self.mult = mult
 		self.mini = mini
 		self.maxi = maxi
 
 	def getRandom(self):
+		if self.even:
+			return self.getEvenNumber()
+		else:
+			return self.getOddNumber()
+
+	def getEvenNumber(self):
 		return 0
+	
+	def getOddNumber(self):
+		return 0
+
+
+
+
--- test.py
+++ test.py
@@ -6,7 +6,7 @@ class Test:
 	
 	@staticmethod
 	def randomNumberTest(r: RandomNumber):
-		numbers = [i for i in range(r.mini, r.maxi) if i % r.mult == 0 and (r.even and i % 2 == 0 or i % 2 == 1)];
+		numbers = [i for i in range(r.mini, r.maxi) if any([i % j == 0 for j in r.mult])];
 		print("Numbers to generate:")
 		print(numbers)
 		
@@ -14,15 +14,36 @@ class Test:
 		while i < Test.MAX_TESTS and len(numbers) > 0:
 			list(filter((r.getRandom()).__ne__, numbers))
 			i += 1
+		print("Test %r %d %d %d " % (r.even, r.mult, r.mini, r.maxi), end = "")
 		if len(numbers) != 0:
-			print("Test %r %d %d %d KO" % (r.even, r.mult, r.mini, r.maxi))
+			Test.print_fail("KO")
 			print("Remaining numbers:")
 			print(numbers)
 		else:
-			print("Test %r %d %d %d OK" % (r.even, r.mult, r.mini, r.maxi))
+			print("OK")
+
+	@staticmethod
+	def print_fail(message, end = '\n'):
+		print('\x1b[1;31m' + message.strip() + '\x1b[0m', end = end)
+
+	@staticmethod
+	def print_pass(message, end = '\n'):
+		sys.stdout.write('\x1b[1;32m' + message.strip() + '\x1b[0m' + end)
+
+	@staticmethod
+	def print_warn(message, end = '\n'):
+		sys.stderr.write('\x1b[1;33m' + message.strip() + '\x1b[0m' + end)
+
+	@staticmethod
+	def print_info(message, end = '\n'):
+		sys.stdout.write('\x1b[1;34m' + message.strip() + '\x1b[0m' + end)
+
+	@staticmethod
+	def print_bold(message, end = '\n'):
+		sys.stdout.write('\x1b[1;37m' + message.strip() + '\x1b[0m' + end)
 
 
 if __name__ == "__main__":
-	r = RandomNumber(even = False, mult = 5, mini = 0, maxi = 200)
+	r = RandomNumber([5], mini = 0, maxi = 200)
 
 	Test.randomNumberTest(r)
