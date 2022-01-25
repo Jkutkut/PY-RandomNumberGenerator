408350488eee40ca2532a421f43ed05448a32be6
Jkutkut <jorgeregonzalez@gmail.com>
2022-01-25 11:54:01 +0100

    basic structure of tests added

--- test.py
+++ test.py
@@ -1,6 +1,13 @@
 from classes.random import RandomNumber
 
+class Test:
 
-r = RandomNumber(even = False, mult = 5, mini = 0, maxi = 200)
+	@staticmethod
+	def randomNumberTest(r: RandomNumber):
+		print(r.getRandom())
 
-print(r.getRandom())
+
+if __name__ == "__main__":
+	r = RandomNumber(even = False, mult = 5, mini = 0, maxi = 200)
+
+	Test.randomNumberTest(r)
