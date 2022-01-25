3a55fd49902636cf57d3031a3098fad2f2816d73
Jkutkut <jorgeregonzalez@gmail.com>
2022-01-25 11:47:26 +0100

    class with logic

---
+++ classes/random.py
@@ -0,0 +1,10 @@
+class RandomNumber:
+
+	def __init__(self, even: bool, mult: int, mini: int, maxi: int):
+		self.even = even
+		self.mult = mult
+		self.mini = mini
+		self.maxi = maxi
+
+	def getRandom(self):
+		return 0
