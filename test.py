5f44b571b408b36c4ae4468635c5c3df3b09df93
Jkutkut <jorgeregonzalez@gmail.com>
2022-01-26 09:21:01 +0100

    test seens to work

--- test.py
+++ test.py
@@ -5,30 +5,32 @@ class Test:
 	MAX_TESTS=1000
 	
 	@staticmethod
-	def randomNumberTest(r: RandomNumber):
-		numbers = [i for i in range(r.mini, r.maxi) if any([i % j == 0 for j in r.mult])];
-		print("Numbers to generate:")
+	def randomNumberTest(rng: RandomNumber):
+		numbers = [i for i in range(rng.mini, rng.maxi) if any([i % j == 0 for j in rng.mult])];
+		print("Numbers to generate:", len(numbers))
 		print(numbers)
 		
 		i = 0
 		while i < Test.MAX_TESTS and len(numbers) > 0:
-			list(filter((r.getRandom()).__ne__, numbers))
+			r = rng.getRandom()
+			if r in numbers:
+				numbers.remove(r)
 			i += 1
-		print("Test %r %d %d %d " % (r.even, r.mult, r.mini, r.maxi), end = "")
+		print("Test %s %d %d %r " % (rng.mult, rng.mini, rng.maxi, rng.odd), end = "")
 		if len(numbers) != 0:
 			Test.print_fail("KO")
 			print("Remaining numbers:")
 			print(numbers)
 		else:
-			print("OK")
+			Test.print_pass("OK")
 
 	@staticmethod
 	def print_fail(message, end = '\n'):
-		sys.stdout.write('\x1b[1;31m' + message.strip() + '\x1b[0m', end = end)
+		print('\x1b[1;31m' + message.strip() + '\x1b[0m', end = end)
 
 	@staticmethod
 	def print_pass(message, end = '\n'):
-		sys.stdout.write('\x1b[1;32m' + message.strip() + '\x1b[0m' + end)
+		print('\x1b[1;32m' + message.strip() + '\x1b[0m' + end)
 
 	@staticmethod
 	def print_warn(message, end = '\n'):
@@ -36,14 +38,14 @@ class Test:
 
 	@staticmethod
 	def print_info(message, end = '\n'):
-		sys.stdout.write('\x1b[1;34m' + message.strip() + '\x1b[0m' + end)
+		print('\x1b[1;34m' + message.strip() + '\x1b[0m' + end)
 
 	@staticmethod
 	def print_bold(message, end = '\n'):
-		sys.stdout.write('\x1b[1;37m' + message.strip() + '\x1b[0m' + end)
+		print('\x1b[1;37m' + message.strip() + '\x1b[0m' + end)
 
 
 if __name__ == "__main__":
-	r = RandomNumber(mult = [5], mini = 0, maxi = 200)
+	r = RandomNumber(mult = [5], mini = 0, maxi = 200, odd = False)
 
 	Test.randomNumberTest(r)
